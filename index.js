4d5d5e0326d609eb86a5c02501d2ede958542d2e
Yarin9Ashmoz <154062144+Yarin9Ashmoz@users.noreply.github.com>
2024-05-30 13:58:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var randomNumber = Math.floor(Math.random()*10 + 1);
+
+var idOffood = document.getElementById(id="foodPic"); 
+
+idOffood.src="./images/food6.jpg".innerHTML = "./images/food"+(randomNumber)+".jpg"
+
